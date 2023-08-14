8c50bcf083886299b25e5058eb328f62bf1f4a7a
VenziUno <pendicai3@gmail.com>
2023-08-14 09:03:38 +0700

    A

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    images: {
+        domains: ["img.jovasoftware.com"],
+    },
+    env: {
+        DOMAIN: process.env.DOMAIN,
+        URL: process.env.URL,
+        API_URL: process.env.API_URL,
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
