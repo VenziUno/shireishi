9adfc35eddae0449bc297fa257e856038c9a0b93
VenziUno <pendicai3@gmail.com>
2023-08-15 13:40:39 +0700

    try

--- components/loader/index.tsx
+++ components/loader/index.tsx
@@ -1,17 +1,15 @@
-import Image from 'next/legacy/image'
+import Image from 'next/image';
+import 'animate.css';
+import { useEffect, useState } from 'react';
 
-import 'animate.css'
+import LogoImage from '../../public/images/logo.png';
 
-// import Logo from "@/images/logo.png";
-import { useEffect, useState } from "react";
 interface Props {
   isLoading: boolean;
 }
 
 export const LandingLoader: React.FC<Props> = ({ isLoading }) => {
-  const [display, setDisplay] = useState(true)
-
-  const Logo = require('../../public/images/logo.png');
+  const [display, setDisplay] = useState(true);
 
   const hide = async (ms: number) => {
     await new Promise((r) => setTimeout(r, ms));
@@ -23,21 +21,19 @@ export const LandingLoader: React.FC<Props> = ({ isLoading }) => {
   }, [isLoading])
 
   return (
-    <>
-      <div
-        className={`animate__animated  fixed inset-0 bg-black transition-all ease-in-out z-[100] duration-1000 flex items-center justify-center ${
-          display ? "opacity-100 visible" : "opacity-0 z-[-100] animate__fadeOut invisible"
-        }`}
-      >
-        <Image
-          className="cursor-pointer"
-          src={Logo}
-          alt="Shireishi"
-          layout="fixed"
-          width={200}
-          height={200}
-        />
-      </div>
-    </>
+    <div
+      className={`animate__animated fixed inset-0 bg-black transition-all ease-in-out z-[100] duration-1000 flex items-center justify-center ${
+        display ? 'opacity-100 visible' : 'opacity-0 z-[-100] animate__fadeOut invisible'
+      }`}
+    >
+      <Image
+        className="cursor-pointer"
+        src={LogoImage}
+        alt="Shireishi"
+        layout="fixed"
+        width={200}
+        height={200}
+      />
+    </div>
   );
 };
